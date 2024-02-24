311662e92c4f15d176fbc178023037e20d7d433b
otakuCoderr <bargavkrishna26@gmail.com>
2024-02-24 22:35:04 +0530

    Added scroll to top

--- script.js
+++ script.js
@@ -28,23 +28,17 @@ function writeText() {
 }
 writeText();
 
-document.addEventListener("DOMContentLoaded", function() {
+window.onscroll = function() {scrollFunction()};
 
-  // Get the button
-  var mybutton = document.getElementsByClassName("scroll-top")[0];
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
 
-  // When the user scrolls down 20px from the top of the document, show the button
-  window.onscroll = function() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      mybutton.style.display = "block";
-    } else {
-      mybutton.style.display = "none";
-    }
-  };
-
-  // When the user clicks on the button, scroll to the top of the document
-  mybutton.onclick = function() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-  };
-});
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
